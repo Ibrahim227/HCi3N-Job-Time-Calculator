3cb996162cf20851d8b2144d7c332720033d93d9
M.Sani Ibrahim <mamansani.ibrahim01@gmail.com>
2023-05-11 08:55:05 +0100

    main code

--- data_entrygui.py
+++ data_entrygui.py
@@ -1,11 +1,11 @@
 import datetime
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, END
 import tkinter as tk
 
 
 class JobTimeCalculator(object):
     def __init__(self):
-        super().__init__()
+        super().__init__()  # Allows to inherit from the tkinter object
         self.window = tk.Tk()
         self.window.title("HCi3N")
         self.window.geometry()
@@ -18,18 +18,28 @@ class JobTimeCalculator(object):
         self.user_info_frame = ttk.Labelframe(self.frame, text='Information Employee', underline=0)
         self.user_info_frame.grid(row=0, column=0, padx=20, pady=20)
 
+        # Create name and last name  labels
         self.first_name_label = ttk.Label(self.user_info_frame, text='Nom:', background="lightgrey", underline=0)
         self.first_name_label.grid(row=0, column=0)
         self.last_name_label = ttk.Label(self.user_info_frame, text='Prenom:', background="lightgrey", underline=0)
         self.last_name_label.grid(row=0, column=1)
 
+        # create entry
         self.first_name_entry = ttk.Entry(self.user_info_frame)
         self.last_name_entry = ttk.Entry(self.user_info_frame)
         self.first_name_entry.grid(row=1, column=0)
         self.last_name_entry.grid(row=1, column=1)
 
-        self.title = ttk.Label(self.user_info_frame, text='Titre:', background="lightgrey", underline=0)
-        self.title_combox = ttk.Combobox(self.user_info_frame, values=["", "Mme.", "M.", "Dr.", "Phd"])
+        # The post combobox
+        self.post_label = ttk.Label(self.user_info_frame, text='Site', background='lightgrey', underline=0)
+        self.post_label.grid(row=2, column=2)
+        self.post_combobox = ttk.Combobox(self.user_info_frame, values=['SIEGE', 'ANNEXE 1', 'ANNEXE 2'], validate='focus')
+        self.post_combobox.grid(row=3, column=2)
+
+        # Create the title combo box
+        self.title = ttk.Label(self.user_info_frame, text='Fonction:', background="lightgrey", underline=0)
+        self.title_combox = ttk.Combobox(self.user_info_frame, values=["", "HAUT-COMMISSAIRE", "SECRETAIRE GENERAL", "SECRETAIRE GENERAL ADJOINT",
+                                                                       "CONSEILLER TECHNIQUE", "DFC", ""])
         self.title.grid(row=0, column=2)
         self.title_combox.grid(row=1, column=2)
 
@@ -38,36 +48,52 @@ class JobTimeCalculator(object):
         self.time_start_label.grid(row=2, column=0)
         self.time_end_label.grid(row=2, column=1)
 
+        # Create Entry widgets
         self.time_start_entry = ttk.Entry(self.user_info_frame)
         self.time_end_entry = ttk.Entry(self.user_info_frame)
         self.time_start_entry.grid(row=3, column=0)
         self.time_end_entry.grid(row=3, column=1)
 
         for widget in self.user_info_frame.winfo_children():
-            widget.grid_configure(padx=10, pady=15, ipadx=10)
+            widget.grid_configure(padx=10, pady=10,)
 
         # Create the second LabelFrame: breakCheck and calculation
         self.reg_status_var = tk.StringVar(value='Pause Ok')
         self.registered_check = ttk.Checkbutton(self.frame, text="Pause", variable=self.reg_status_var, onvalue="Pause Ok", offvalue="Pause No")
-        self.registration_frame = ttk.LabelFrame(self.frame, text='Pause & Calcule', underline=0)
+        self.registration_frame = ttk.LabelFrame(self.frame, text='Pause & Affichage', underline=0)
         self.registration_frame.grid(row=1, column=0, sticky='news', padx=20, pady=20)
 
         self.break_check_button_var = tk.BooleanVar(self.registration_frame, value=True)
         self.break_check_button = ttk.Checkbutton(self.registration_frame, text="Pause entre 13:00 PM et 15:00 PM", variable=self.break_check_button_var)
         self.break_check_button.grid(row=2, column=0)
 
+        # create calculate button
         self.calculate_button = ttk.Button(self.registration_frame, text="Calculer", command=self.calculate)
         self.calculate_button.grid(row=2, column=1, sticky='news')
 
+        # result view label
         self.result_label = ttk.Label(self.registration_frame, background='lightgreen')
-        self.result_label.grid(row=2, column=2, sticky='news')
+        self.result_label.grid(row=2, column=3, sticky='news')
 
         for widget in self.registration_frame.winfo_children():
-            widget.grid_configure(padx=10, pady=15, ipadx=10)
+            widget.grid_configure(padx=10, pady=15, ipadx=7)
 
         # Create generate Button
         self.entry_button = ttk.Button(self.frame, text='Generer', command=self.generate)
         self.entry_button.grid(row=3, column=0, sticky='news', padx=20, pady=5)
+
+        # add a clear button
+        self.clear_button = ttk.Button(self.frame, text='Effacer', command=self.clear)
+        self.clear_button.grid(row=2, column=0, sticky='nw', padx=20, pady=5)
+
+    # Clear function
+    def clear(self):
+        self.first_name_entry.delete(0, END)
+        self.last_name_entry.delete(0, END)
+        self.post_combobox.delete(0, END)
+        self.title_combox.delete(0, END)
+        self.time_start_entry.delete(0, END)
+        self.time_end_entry.delete(0, END)
 
         # Calculate function
     def calculate(self):
